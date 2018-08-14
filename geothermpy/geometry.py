daae5f291e85ae115e877b3f981d8cd41c8f7047
Qi Zhang <singularitti@outlook.com>
2018-08-14 15:46:14 -0400

    Add SurfacePoint to geometry.py

--- geothermpy/geometry.py
+++ geothermpy/geometry.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
 
+import typing
 from collections import namedtuple
 
 __all__ = [
     'Point',
     'Rectangle',
-    'rectangle_to_points'
+    'SurfacePoint',
+    'rectangle_to_points',
+    'within_rectangle',
+    'point_to_surface_point'
 ]
 
 Point = namedtuple('Point', ['x', 'y'])
 Rectangle = namedtuple('Rectangle', ['lx', 'ly', 'rx', 'ry'])
+SurfacePoint = namedtuple('SurfacePoint', ['x', 'y', 'z'])
 
 
-def rectangle_to_points(rec: Rectangle):
+def rectangle_to_points(rec: Rectangle) -> typing.Tuple[Point, ...]:
     lx, rx, ly, ry = rec.lx, rec.ly, rec.rx, rec.ry
     return Point(lx, ly), Point(lx, ry), Point(rx, ly), Point(rx, ry)
+
+
+def within_rectangle(rec: Rectangle, t: Point) -> bool:
+    lx, rx, ly, ry = rec.lx, rec.ly, rec.rx, rec.ry
+    x, y = t.x, t.y
+    return lx <= x <= rx and ly <= y <= ry
+
+
+def point_to_surface_point(p: Point, z: float) -> SurfacePoint:
+    return SurfacePoint(p.x, p.y, z)
