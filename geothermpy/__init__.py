4df75cf512b05e812f2b04f646fa96de9ea85c4d
Qi Zhang <singularitti@outlook.com>
2018-08-14 18:53:59 -0400

    Modify `bind` based on `GeothermalGradient`

--- geothermpy/__init__.py
+++ geothermpy/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from .geometry import *
+from .gradient import *
 from .integrate import *
 from .interpolate import *
 from .load_data import *
--- geothermpy/bind.py
+++ geothermpy/bind.py
@@ -2,22 +2,25 @@
 
 import numpy as np
 
-from geothermpy import Point, runge_kutta_iter, point_to_surface_point, bilinear_interpolate
+from geothermpy import Point, runge_kutta_iter, point_to_surface_point, bilinear_interpolate, GeothermalGradient
 
 
 def find_nearest(arr: np.array, x):
     return np.argmin(abs(arr - x))
 
 
-def bind(adiabat, ts, ps, p0: Point, h=0.01, n=1000):
+def bind(gg: GeothermalGradient, p0: Point, h=0.01, n=1000):
+    ts = gg.temperatures
+    ps = gg.pressures
+    geothermal_gradient = gg.gradient.values
     trace = [p0]
     for k in range(n):
         i, j = find_nearest(ts, trace[k].x), find_nearest(ps, trace[k].y)
         f = bilinear_interpolate(
-            point_to_surface_point(Point(ts[i], ps[j]), adiabat[i, j]),
-            point_to_surface_point(Point(ts[i], ps[j + 3]), adiabat[i, j + 3]),
-            point_to_surface_point(Point(ts[i + 3], ps[j]), adiabat[i + 3, j]),
-            point_to_surface_point(Point(ts[i + 3], ps[j + 3]), adiabat[i + 3, j + 3])
+            point_to_surface_point(Point(ts[i], ps[j]), geothermal_gradient[i, j]),
+            point_to_surface_point(Point(ts[i], ps[j + 3]), geothermal_gradient[i, j + 3]),
+            point_to_surface_point(Point(ts[i + 3], ps[j]), geothermal_gradient[i + 3, j]),
+            point_to_surface_point(Point(ts[i + 3], ps[j + 3]), geothermal_gradient[i + 3, j + 3])
         )
         p_next = runge_kutta_iter(trace[k], f, h)
         trace.append(p_next)
