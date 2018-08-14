297b92995272707f49d1ce533feb39353a47b8d4
Qi Zhang <singularitti@outlook.com>
2018-08-14 16:02:36 -0400

    Rewrite integrate.py

--- geothermpy/integrate.py
+++ geothermpy/integrate.py
@@ -1,45 +1,16 @@
 #!/usr/bin/env python
 
-import numpy as np
-from geothermpy import Point, bilinear_interpolate, Rectangle
+from geothermpy import Point
+
+__all__ = [
+    'runge_kutta_iter'
+]
 
 
-def boundary_check(p: Point, adiabat: np.ndarry):
-    c, k = adiabat.shape
-    temperature, pressure = p.x, p.y
-    x, y, z, w = [1] * 4
-
-    for i in range(c):
-        if adiabat(i, 0) <= temperature:
-            x = i
-    for j in range(k):
-        if adiabat(0, j) <= pressure:
-            y = j
-
-    if adiabat(0, y) == pressure and adiabat(x, 0) == temperature:
-        return adiabat(x, y)
-
-    if x < c:
-        z = x + 1
-    elif x == c:
-        z = x - 1
-    else:
-        raise IndexError
-
-    if y < k:
-        w = y + 1
-    elif y == k:
-        w = y - 1
-    else:
-        raise IndexError
-
-    return x, y, z, w
-
-
-def interp(p: Point, adiabat: np.ndarry):
-    x, y, z, w = boundary_check(p, adiabat)
-    return bilinear_interpolate(Rectangle(adiabat(x, 1),))
-
-
-def runge_kutta(p: Point, h=0.01):
-    pass
+def runge_kutta_iter(p: Point, f, h=0.01) -> Point:
+    x, y = p.x, p.y
+    k1 = h * f(x, y)
+    k2 = h * f(x + h / 2, y + k1 / 2)
+    k3 = h * f(x + h / 2, y + k2 / 2)
+    k4 = h * f(x + h, y + k3)
+    return Point(x + h, y + (k1 + 2 * k2 + 2 * k3 + k4) / 6)
