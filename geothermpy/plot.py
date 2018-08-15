e174792845eb0ca3f2a736bc20ecf0871589b9a9
Qi Zhang <singularitti@outlook.com>
2018-08-15 17:51:10 -0400

    Rewrite plot.py

--- geothermpy/plot.py
+++ geothermpy/plot.py
@@ -10,5 +10,9 @@ __all__ = [
 def plot_trace(points):
     xs = list(map(lambda x: x.x, points))
     ys = list(map(lambda x: x.y, points))
-    plt.plot(xs, ys)
-    plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(ys, xs)
+    ax.set_xlabel(r"$P$ (GPa)")
+    ax.set_ylabel(r"$T$ (K)")
+    ax.set_title(r"adiabatic $T(P)$")
+    return fig, ax
