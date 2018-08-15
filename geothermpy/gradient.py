Fix a bug `gradient` property doesn't work

geothermpy/gradient.py
@@ -10,17 +10,15 @@ __all__ = [
 
 class GeothermalGradient:
     def __init__(self, alpha, volume, cp):
-        self.alpha = alpha.values
-        self.volume = volume.values
-        self.cp = cp.values
+        self.alpha = alpha
+        self.volume = volume
+        self.cp = cp
         self.temperatures = alpha.iloc[:, 0]
         self.pressures = np.array(alpha.columns[1:], dtype=float)
-        self._gradient = None
 
     def calculate_gradient(self):
-        df = pd.DataFrame(((self.alpha * self.volume / self.cp).T * self.temperatures).T, columns=self.pressures)
-        self._gradient = df.insert(0, 'T (K)', self.temperatures)
+        return ((self.alpha * self.volume / self.cp).T * self.temperatures).T
 
     @property
     def gradient(self):
-        return self._gradient
+        return self.calculate_gradient()