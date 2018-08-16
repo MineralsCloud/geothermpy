4ccec18be1ee1d51a20bc2905e3e2f255b97cf66
Qi Zhang <singularitti@outlook.com>
2018-08-15 23:37:04 -0400

    docs: Add doc for `runge_kutta_iter`

--- geothermpy/integrate.py
+++ geothermpy/integrate.py
@@ -8,6 +8,23 @@ __all__ = [
 
 
 def runge_kutta_iter(p: Point, f, h=0.01) -> Point:
+    """
+    Integrate an initial value problem
+
+    .. math::
+
+       \begin{align}
+           y(x_0) &= y_0, \\
+           \\frac{ dy }{ dx } &= f(x, y),
+       \end{align}
+
+    where :math:`y` is an unknown function (scalar or vector) of variable :math:`x`.
+
+    :param p: A point that specifies the initial value, i.e., :math:`(x_0, y_0)`.
+    :param f: A bivariate function of :math:`x` and :math:`y`, which specifies the ODE.
+    :param h: The integration time step, by default is ``0.01``.
+    :return: A point :math:`(x, y)` of the next time step.
+    """
     x, y = p.x, p.y
     k1 = h * f(x, y)
     k2 = h * f(x + h / 2, y + k1 / 2)
