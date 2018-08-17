8a3a1f10b6711f5570a819fe7712712481250552
Qi Zhang <singularitti@outlook.com>
2018-08-17 02:57:11 -0400

    Know what's causing the error and remove old implementation
    It is because the Runge--Kutta method will interpolate values that
    are greater than the rectangle boundary.

--- geothermpy/bind.py
+++ geothermpy/bind.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import numpy as np
 import bisect
 
-from geothermpy import Point, runge_kutta_iter, bilinear_interpolate, SurfacePoint
 import pandas as pd
+
+from geothermpy import Point, runge_kutta_iter, bilinear_interpolate, SurfacePoint
 
 
 def find_le(arr, x):
@@ -21,54 +21,17 @@ def find_le(arr, x):
     raise ValueError("The argument *arr* is not sorted, the algorithm might not work!")
 
 
-def boundary_check(p: Point, adiabat: np.array):
-    c, k = adiabat.shape
-    temperature, pressure = p.x, p.y
-    x, y, z, w = [1] * 4
-
-    for i in range(c):
-        if adiabat[i, 0] <= temperature:
-            x = i
-    for j in range(k):
-        if adiabat[0, j] <= pressure:
-            y = j
-
-    if p.x == pressure and p.y == temperature:
-        return adiabat[x, y]
-
-    if x == c - 1 and y < k - 1:
-        z = x - 1
-        w = y + 1
-    elif x < c - 1 and y == k - 1:
-        z = x + 1
-        w = y - 1
-    elif x == c - 1 and y < k - 1:
-        z = x - 1
-        w = y - 1
-    elif x < c - 1 and y < k - 1:
-        z = x + 1
-        w = y + 1
-
-    return x - 1, y - 1, z - 1, w - 1
-
-
 def bind(geothermal_gradient: pd.DataFrame, p0: Point, h=0.01, n=1000):
     ts = geothermal_gradient.index.astype('float')
     ps = geothermal_gradient.columns.astype('float')
     trace = [p0]
     for k in range(n):
-        x, y = find_le(ps, trace[k].x), find_le(ts, trace[k].y)
-        if x == len(ps) - 1:
-            x = len(ps) - 2
-        z = x + 2
-        if y == len(ts) - 1:
-            y = len(ts) - 2
-        w = y + 2
+        i, j = find_le(ps, trace[k].x), find_le(ts, trace[k].y)
         f = bilinear_interpolate(
-            SurfacePoint(ps[x], ts[y], geothermal_gradient.iloc[y, x]),
-            SurfacePoint(ps[x], ts[w], geothermal_gradient.iloc[w, x]),
-            SurfacePoint(ps[z], ts[y], geothermal_gradient.iloc[y, z]),
-            SurfacePoint(ps[z], ts[w], geothermal_gradient.iloc[w, z])
+            SurfacePoint(ps[i], ts[j], geothermal_gradient.iloc[j, i]),
+            SurfacePoint(ps[i], ts[j + 1], geothermal_gradient.iloc[j + 1, i]),
+            SurfacePoint(ps[i + 1], ts[j], geothermal_gradient.iloc[j, i + 1]),
+            SurfacePoint(ps[i + 1], ts[j + 1], geothermal_gradient.iloc[j + 1, i + 1])
         )
         p_next = runge_kutta_iter(trace[k], f, h)
         trace.append(p_next)
