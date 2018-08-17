a68535f84f6fc729c2f57bf06899545cf749eb6c
Qi Zhang <singularitti@outlook.com>
2018-08-17 03:09:46 -0400

    Change the implementation of `bind`

--- geothermpy/bind.py
+++ geothermpy/bind.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import bisect
+from bisect import bisect_right
 
 import pandas as pd
 
@@ -9,15 +9,29 @@ from geothermpy import Point, runge_kutta_iter, bilinear_interpolate, SurfacePoi
 
 def find_le(arr, x):
     """
-    Find rightmost value less than or equal to *x*.
+    Find the rightmost index whose value is less than or equal to *x*.
 
     :param arr:
     :param x:
     :return:
     """
-    i = bisect.bisect_right(arr, x)
+    i = bisect_right(arr, x)
     if i:
         return i - 1
+    raise ValueError("The argument *arr* is not sorted, the algorithm might not work!")
+
+
+def find_gt(arr, x):
+    """
+    Find the leftmost index whose value is greater than *x*.
+
+    :param arr:
+    :param x:
+    :return:
+    """
+    i = bisect_right(arr, x)
+    if i != len(arr):
+        return i
     raise ValueError("The argument *arr* is not sorted, the algorithm might not work!")
 
 
@@ -25,15 +39,16 @@ def bind(geothermal_gradient: pd.DataFrame, p0: Point, h=0.01, n=1000):
     ts = geothermal_gradient.index.astype('float')
     ps = geothermal_gradient.columns.astype('float')
     trace = [p0]
-    for k in range(n):
-        i, j = find_le(ps, trace[k].x), find_le(ts, trace[k].y)
+    for m in range(n):
+        i, j = find_le(ps, trace[m].x), find_le(ts, trace[m].y)
+        k, l = find_gt(ps, trace[m].x), find_gt(ts, trace[m].y)
         f = bilinear_interpolate(
             SurfacePoint(ps[i], ts[j], geothermal_gradient.iloc[j, i]),
-            SurfacePoint(ps[i], ts[j + 1], geothermal_gradient.iloc[j + 1, i]),
-            SurfacePoint(ps[i + 1], ts[j], geothermal_gradient.iloc[j, i + 1]),
-            SurfacePoint(ps[i + 1], ts[j + 1], geothermal_gradient.iloc[j + 1, i + 1])
+            SurfacePoint(ps[i], ts[l], geothermal_gradient.iloc[l, i]),
+            SurfacePoint(ps[k], ts[j], geothermal_gradient.iloc[j, k]),
+            SurfacePoint(ps[k], ts[l], geothermal_gradient.iloc[l, k])
         )
-        p_next = runge_kutta_iter(trace[k], f, h)
+        p_next = runge_kutta_iter(trace[m], f, h)
         trace.append(p_next)
     return trace
 
