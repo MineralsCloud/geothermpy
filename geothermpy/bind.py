795f35fbcd5552b6e43bcd92c89b7e82a6c5b058
Qi Zhang <singularitti@outlook.com>
2018-08-18 15:11:54 -0400

    Remove useless `IndexError` check
    and change index names

--- geothermpy/bind.py
+++ geothermpy/bind.py
@@ -33,15 +33,13 @@ def find_lower_bounds(xs, ys) -> Callable:
 
 def inject_find_lower_bound(ps, ts, geothermal_gradient) -> Callable:
     def g(x, y):
-        i, j = find_lower_bounds(ps, ts)(x, y)
-        k, l = i + 1, j + 1
-        if k == len(ps) or l == len(ts):
-            raise IndexError("The index has reached the last of pressures or temperatures!")
+        m, n = find_lower_bounds(ps, ts)(x, y)
+        o, p = m + 1, n + 1
         interpolated_function: Callable = bilinear_interpolate(
-            SurfacePoint(ps[i], ts[j], geothermal_gradient[j, i]),
-            SurfacePoint(ps[i], ts[l], geothermal_gradient[l, i]),
-            SurfacePoint(ps[k], ts[j], geothermal_gradient[j, k]),
-            SurfacePoint(ps[k], ts[l], geothermal_gradient[l, k])
+            SurfacePoint(ps[m], ts[n], geothermal_gradient[n, m]),
+            SurfacePoint(ps[m], ts[p], geothermal_gradient[p, m]),
+            SurfacePoint(ps[o], ts[n], geothermal_gradient[n, o]),
+            SurfacePoint(ps[o], ts[p], geothermal_gradient[p, o])
         )
         return interpolated_function(x, y)
 
@@ -52,10 +50,10 @@ def bind(geothermal_gradient: pd.DataFrame, p0: Point, h=0.01, n=1000):
     ts = geothermal_gradient.index.astype('float').values
     ps = geothermal_gradient.columns.astype('float').values
     trace = [p0]
-    for m in range(n):
+    for i in range(n):
         g = inject_find_lower_bound(ps, ts, geothermal_gradient.values)
         try:
-            p_next = runge_kutta_iter(trace[m], g, h)
+            p_next = runge_kutta_iter(trace[i], g, h)
         except IndexError:
             return trace
         trace.append(p_next)
