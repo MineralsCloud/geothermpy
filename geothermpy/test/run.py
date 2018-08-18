e4f46d515f7c36c10421663617b361b1dac903d0
Qi Zhang <singularitti@outlook.com>
2018-08-18 11:30:08 -0400

    Merge run.py and plot.py as test example
    I do not want a plotting module so I removed plot.py here.

--- geothermpy/test/run.py
+++ geothermpy/test/run.py
@@ -3,24 +3,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from geothermpy import bind, Point, plot, load_geothermal_gradient
+from geothermpy import bind, Point, load_geothermal_gradient
 
 if __name__ == '__main__':
-    # d = load_csv({
-    #     'alpha': '5000_alpha_tp.csv',
-    #     'cp': '5000_cp_tp.csv',
-    #     'v': '5000_v_tp.csv'
-    # })
-    #
-    # alpha = d['alpha']
-    # cp = d['cp']
-    # v = d['v']
-    # ts = v.iloc[:, 0]
+    gg = load_geothermal_gradient('adiabat 2.csv')
+    trace = bind.bind(gg, Point(24, 1876.80005), h=0.01, n=100000)
+    xs = list(map(lambda x: x.x, trace))
+    ys = list(map(lambda x: x.y, trace))
 
-    # gg = GeothermalGradient(alpha, v, cp)
-    gg = load_geothermal_gradient('adiabat.csv')
-    trace = bind.bind(gg, Point(55, 1717), n=6000)
-    fig, ax = plot.plot_trace(trace)
-    ref = pd.read_csv('data.csv')
-    ax.plot(ref.loc[:, 'P(GPa)'], ref.loc[:, 'T(K)'])
+    fig, ax = plt.subplots()
+    ax.plot(xs, ys, label="my result")
+
+    ref = pd.read_csv('data 2.csv')
+    ax.plot(ref.loc[:, 'P(GPa)'], ref.loc[:, 'T(K)'], label="renata result")
+
+    ax.set_xlabel(r"$P$ (GPa)")
+    ax.set_ylabel(r"$T$ (K)")
+    ax.legend(loc="best")
+    ax.set_title(r"adiabatic $T(P)$")
+    fig.savefig('run.pdf')
     plt.show()
