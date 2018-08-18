7cebd49a31f673241a21ab28bf2221f9faee9f5c
Qi Zhang <singularitti@outlook.com>
2018-08-18 19:58:37 -0400

    Temporarily remove unused modules

--- geothermpy/load_data.py
+++ geothermpy/load_data.py
@@ -3,16 +3,8 @@
 import pandas as pd
 
 __all__ = [
-    'load_csv',
     'load_geothermal_gradient'
 ]
-
-
-def load_csv(d: dict) -> dict:
-    for k, v in d.items():
-        m = pd.read_csv(v)
-        d.update({k: m.set_index('Unnamed: 0')})
-    return d
 
 
 def load_geothermal_gradient(file: str):
--- geothermpy/test/generate_trace.py
+++ geothermpy/test/generate_trace.py
@@ -7,7 +7,7 @@ from geothermpy import bind, Point, load_geothermal_gradient
 
 if __name__ == '__main__':
     gg = load_geothermal_gradient('example.csv')
-    trace = bind.generate_trace(gg, Point(24, 1876.80005), h=0.01, n=1000)
+    trace = bind.generate_trace(gg, Point(24, 1876.80005), h=0.01, n=100000)
     xs = list(map(lambda x: x.x, trace))
     ys = list(map(lambda x: x.y, trace))
 
