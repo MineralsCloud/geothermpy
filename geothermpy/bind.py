ac27a9aeea82b66e2e4af0ed823c4eb4a495f40c
Qi Zhang <singularitti@outlook.com>
2018-08-17 23:26:13 -0400

    refactor: Auto-handle `IndexError`
    which is caused by reaching the end of pressure-vector
    or temperature-vector when the integration has gone
    too far.

--- geothermpy/bind.py
+++ geothermpy/bind.py
@@ -10,13 +10,16 @@ from geothermpy import Point, SurfacePoint, bilinear_interpolate, runge_kutta_it
 def inject_find_lower_bound(ps, ts, geothermal_gradient) -> Callable:
     def g(x, y):
         i, j = find_lower_bound(ps, ts)(x, y)
-        interpolated: Callable = bilinear_interpolate(
+        k, l = i + 1, j + 1
+        if k == len(ps) or l == len(ts):
+            raise IndexError("The index has reached the last of pressures or temperatures!")
+        interpolated_function: Callable = bilinear_interpolate(
             SurfacePoint(ps[i], ts[j], geothermal_gradient[j, i]),
-            SurfacePoint(ps[i], ts[j + 1], geothermal_gradient[j + 1, i]),
-            SurfacePoint(ps[i + 1], ts[j], geothermal_gradient[j, i + 1]),
-            SurfacePoint(ps[i + 1], ts[j + 1], geothermal_gradient[j + 1, i + 1])
+            SurfacePoint(ps[i], ts[l], geothermal_gradient[l, i]),
+            SurfacePoint(ps[k], ts[j], geothermal_gradient[j, k]),
+            SurfacePoint(ps[k], ts[l], geothermal_gradient[l, k])
         )
-        return interpolated(x, y)
+        return interpolated_function(x, y)
 
     return g
 
@@ -27,6 +30,9 @@ def bind(geothermal_gradient: pd.DataFrame, p0: Point, h=0.01, n=1000):
     trace = [p0]
     for m in range(n):
         g = inject_find_lower_bound(ps, ts, geothermal_gradient.values)
-        p_next = runge_kutta_iter(trace[m], g, h)
+        try:
+            p_next = runge_kutta_iter(trace[m], g, h)
+        except IndexError:
+            return trace
         trace.append(p_next)
     return trace
