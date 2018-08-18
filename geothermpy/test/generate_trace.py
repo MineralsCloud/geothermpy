a7517a52f96cfd3db31e7124b329fd7e8efff8a7
Qi Zhang <singularitti@outlook.com>
2018-08-18 16:43:39 -0400

    test: Add original Fortran version test

--- geothermpy/test/generate_trace.py
+++ geothermpy/test/generate_trace.py
@@ -6,20 +6,19 @@ import pandas as pd
 from geothermpy import bind, Point, load_geothermal_gradient
 
 if __name__ == '__main__':
-    gg = load_geothermal_gradient('adiabat 2.csv')
-    trace = bind.bind(gg, Point(24, 1876.80005), h=0.01, n=100000)
+    gg = load_geothermal_gradient('example.csv')
+    trace = bind.generate_trace(gg, Point(24, 1876.80005), h=0.01, n=1000)
     xs = list(map(lambda x: x.x, trace))
     ys = list(map(lambda x: x.y, trace))
 
     fig, ax = plt.subplots()
-    ax.plot(xs, ys, label="my result")
+    ax.plot(xs, ys, label="Python version result")
 
-    ref = pd.read_csv('data 2.csv')
-    ax.plot(ref.loc[:, 'P(GPa)'], ref.loc[:, 'T(K)'], label="renata result")
+    ref = pd.read_csv('out.csv')
+    ax.plot(ref.loc[:, 'P(GPa)'], ref.loc[:, 'T(K)'], label="Fortran version result")
 
     ax.set_xlabel(r"$P$ (GPa)")
     ax.set_ylabel(r"$T$ (K)")
     ax.legend(loc="best")
     ax.set_title(r"adiabatic $T(P)$")
-    fig.savefig('run.pdf')
     plt.show()
