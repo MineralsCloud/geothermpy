0b9a2d9a8002075379cf58b0a68dbc3db4fb7974
Qi Zhang <singularitti@outlook.com>
2018-08-18 15:31:24 -0400

    Change attribute name of `Rectangle` class

--- geothermpy/geometry.py
+++ geothermpy/geometry.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import typing
 from collections import namedtuple
 
 __all__ = [
@@ -16,22 +15,22 @@ SurfacePoint = namedtuple('SurfacePoint', ['x', 'y', 'z'])
 
 
 class Rectangle:
-    def __init__(self, lx, rx, dy, uy):
+    def __init__(self, lx, rx, ly, uy):
         self.lx = min(lx, rx)
         self.rx = max(lx, rx)
-        self.dy = min(dy, uy)
-        self.uy = max(dy, uy)
+        self.ly = min(ly, uy)
+        self.uy = max(ly, uy)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.lx}, {self.rx}, {self.dy}, {self.uy})"
+        return f"{self.__class__.__name__}({self.lx}, {self.rx}, {self.ly}, {self.uy})"
 
 
-def rectangle_to_points(rec: Rectangle) -> typing.Tuple[Point, ...]:
-    lx, rx, dy, uy = rec.lx, rec.rx, rec.dy, rec.uy
+def rectangle_to_points(rec: Rectangle):
+    lx, rx, dy, uy = rec.lx, rec.rx, rec.ly, rec.uy
     return Point(lx, dy), Point(lx, uy), Point(rx, dy), Point(rx, uy)
 
 
 def within_rectangle(rec: Rectangle, t: Point) -> bool:
-    lx, rx, dy, uy = rec.lx, rec.rx, rec.dy, rec.uy
+    lx, rx, dy, uy = rec.lx, rec.rx, rec.ly, rec.uy
     x, y = t.x, t.y
     return lx <= x <= rx and dy <= y <= uy
