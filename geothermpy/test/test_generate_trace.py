ba735cde059379ad1cfa12c01417e1ae0f44f290
Qi Zhang <singularitti@outlook.com>
2018-08-19 00:52:23 -0400

    fix: Rename generate_trace.py and fix an `FileNotFoundError`
    which was left over by commit "207649f4b501d97ec21d4ab614121c06675bbad1".

--- geothermpy/test/test_generate_trace.py
+++ geothermpy/test/test_generate_trace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from geothermpy import bind, Point, load_geothermal_gradient
@@ -8,13 +9,12 @@ from geothermpy import bind, Point, load_geothermal_gradient
 if __name__ == '__main__':
     gg = load_geothermal_gradient('data/example.csv')
     trace = bind.generate_trace(gg, Point(24, 1876.80005), h=0.01, n=100000)
-    xs = list(map(lambda x: x.x, trace))
-    ys = list(map(lambda x: x.y, trace))
+    xs, ys = np.array([p.x for p in trace]), np.array([p.y for p in trace])
 
     fig, ax = plt.subplots()
     ax.plot(xs, ys, label="Python version result")
 
-    ref = pd.read_csv('out.csv')
+    ref = pd.read_csv('data/out.csv')
     ax.plot(ref.loc[:, 'P(GPa)'], ref.loc[:, 'T(K)'], label="Fortran version result")
 
     ax.set_xlabel(r"$P$ (GPa)")
